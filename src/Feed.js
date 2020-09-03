dda36f3f4d4488d4307f83663f43000bb1b6a044
Shon0221 <yoy285437@gmail.com>
2020-09-03 11:33:20 +0800

    Remove import Post on Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
-import Post from './Post';
 
 function Feed(props) {
     return (
